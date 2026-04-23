3f465d9d784bb4d6e13f8aaf830e868b378ac503
Jervin Carlos <jlcarloswork@gmail.com>
2026-04-23 23:01:26 +0800

    Fix typing

--- src/config/supabase.config.ts
+++ src/config/supabase.config.ts
@@ -1,9 +1,9 @@
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { ConfigService } from '@nestjs/config';
+import { createClient } from '@supabase/supabase-js';
 
 export const SUPABASE_CLIENT = 'SUPABASE_CLIENT';
 
-export function createSupabaseClient(configService: ConfigService): SupabaseClient {
+export function createSupabaseClient(configService: ConfigService) {
   const url = configService.getOrThrow<string>('SUPABASE_URL');
   const key = configService.getOrThrow<string>('SUPABASE_SERVICE_ROLE_KEY');
   return createClient(url, key);
